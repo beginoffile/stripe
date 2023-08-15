ab6d3375f31724a92071162e721bb4de35e568fe
beginoffile <cursos.mapatupisaro@gmail.com>
2023-08-15 13:57:44 -0600

    fix strip_key

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,11 +7,11 @@ import (
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
 
-	stringMap := make(map[string]string)
-	stringMap["publishable_key"] = app.config.stripe.key
+	// stringMap := make(map[string]string)
+	// stringMap["publishable_key"] = app.config.stripe.key
 
 	if err := app.renderTemplate(w, r, "terminal", &templateData{
-		StringMap: stringMap,
+		// StringMap: stringMap,
 	}, "stripe-js"); err != nil {
 		app.errorLog.Println(err)
 
